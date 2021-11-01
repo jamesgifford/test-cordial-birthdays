ec7cbb08c0410e426208f653d365d2a5b668efb5
James Gifford <jg.webdeveloper@gmail.com>
2021-11-01 02:21:35 -0700

    Cleanup 404 response with ModelNotFoundException

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -49,6 +49,10 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Throwable $exception)
     {
+        if ($exception instanceof ModelNotFoundException) {
+            return response(null, 404);
+        }
+
         return parent::render($request, $exception);
     }
 }
